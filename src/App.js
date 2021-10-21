6c3c198527d63eddabc588a58f53ac2842e8224d
Ali Goergen <alyssa.goergen@gmail.com>
2021-10-20 22:36:10 -0400

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const DashboardPage = () => {
+  return (
+    <div>this is a dashboard component</div>
+  )
+}
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <Routes/> */}
+//         <h1>hello world</h1>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
- 
-        <h1>hello world</h1>
-      </header>
+      <BrowserRouter>
+        <Route path="/">
+          <DashboardPage/>
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
